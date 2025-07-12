c04d2c833476fc4d3a527b05cc78d96cafacabc4
Jaydeep Patil <jaydeeppatil3704@gmail.com>
2025-07-12 18:20:08 +0530

    adds no applicants component

--- src/app/(mainLayout)/(organization)/my-jobs/[id]/applications/page.tsx
+++ src/app/(mainLayout)/(organization)/my-jobs/[id]/applications/page.tsx
@@ -1,10 +1,11 @@
 import { Button, buttonVariants } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "@/components/ui/dropdown-menu"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { cn } from "@/lib/utils"
 import { getCurrentUser } from "@/utils/currentUser"
 import { prisma } from "@/utils/db"
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
-import { MoreHorizontal } from "lucide-react"
+import { MoreHorizontal, UserX } from "lucide-react"
 import Link from "next/link"
 import { redirect } from "next/navigation"
 
@@ -41,9 +42,12 @@ export default async function JobApplicationsPage({ params }: { params: Promise<
         redirect('/')
     }
     const applications = await getApplications(id)
+    if(applications.length==0){
+        return <NoApplicants/>
+    }
     return (
         <div>
-
+            <h1 className="text-2xl mb-5 font-semibold">Job Applications</h1>
             <Table>
                 <TableHeader>
                     <TableRow>
@@ -105,4 +109,24 @@ function ActionsDropdown({application}) {
             </DropdownMenuContent>
         </DropdownMenu>
     )
+}
+
+
+
+ function NoApplicants() {
+  return (
+    <div className="w-full py-12 flex flex-col items-center justify-center text-center">
+      <UserX className="h-12 w-12 text-muted-foreground mb-4" />
+      <h2 className="text-2xl font-semibold mb-2 text-muted-foreground">No Applicants Yet</h2>
+      <p className="text-muted-foreground max-w-md">
+        This job hasn't received any applications yet. You can share the job link to reach more potential candidates.
+      </p>
+      <Link
+        href={'/my-jobs'}
+        className={cn(buttonVariants({variant:'default'}),'mt-5')}
+      >
+        Back to My Job Listings
+      </Link>
+    </div>
+  );
 }
